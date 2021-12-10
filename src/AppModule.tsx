9c51f5e9b26c344b4dd595812ca39275663433b6
Matej Mok <matej.mok@gmail.com>
2021-12-10 19:05:48 +0100

    modularization

---
+++ src/AppModule.tsx
@@ -0,0 +1,21 @@
+import React, { useState } from 'react';
+import AppContainer from "./redux/containers/AppContainer";
+import {createStore} from "redux";
+import reducer from "./redux/reducers";
+import {enableMapSet} from "immer";
+
+enableMapSet();
+
+function initStore() {
+    return createStore(reducer, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
+}
+
+function AppModule() {
+    // @ts-ignore
+    const store = useState(initStore())[0];
+    return (
+        <AppContainer store={store}/>
+    );
+}
+
+export default AppModule;
