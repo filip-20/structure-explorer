cbfba138d5ff177d763661225ae883e600ae6896
Matej Mok <matej.mok@gmail.com>
2022-01-03 23:48:09 +0100

    updated module interface (getting app state)

--- src/AppModule.tsx
+++ src/AppModule.tsx
@@ -3,19 +3,37 @@ import AppContainer from "./redux/containers/AppContainer";
 import {createStore} from "redux";
 import reducer from "./redux/reducers";
 import {enableMapSet} from "immer";
+import { stateToJSON } from './stateToJSON';
+import { importAppState } from './redux/actions';
 
 enableMapSet();
 
-function initStore() {
-    // @ts-ignore
-    return createStore(reducer, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
+function prepare(initialState: any) {
+    const instance = {
+        // @ts-ignore
+        store: createStore(reducer, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
+    };
+    const getState = (instance: any) => stateToJSON(instance.store.getState());
+    
+    if (initialState !== null) {
+      instance.store.dispatch(importAppState(initialState, instance.store.getState().diagramState));
+    }
+    
+    return {
+        instance,
+        getState,
+    };
 }
 
-function AppModule() {
-    const store = useState(initStore())[0];
+function AppComponent(props: {instance: any, onStateChange: () => void}) {
+    const store = props.instance.store;
+    store.subscribe(() => props.onStateChange())
     return (
         <AppContainer store={store}/>
     );
 }
 
-export default AppModule;
+export {
+  prepare,
+  AppComponent,
+};
